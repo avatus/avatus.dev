9e147fab2a19bf3e63f8437eff6fd881b3974441
Michael <michaelzmyers@gmail.com>
2022-05-28 01:01:11 -0500

    updating about

--- src/utils/bin/about.ts
+++ src/utils/bin/about.ts
@@ -5,7 +5,7 @@ I build distributed, web-based applications.
 I'm an open-source advocate, fan of all-things-tech, and ttrpg nerd.
 I like to spend my free time <a class="link__teleport" href="https://github.com/avatus" target="_blank">coding</a>, <a class="link__teleport" href="https://soundcloud.com/avatus" target="_blank">making music</a>, and travelling to Japan.
 
-My current interests involve Elixir, GraphQL federation, and RTC.
+My current interests include Elixir, GraphQL federation, and RTC.
 For further communication, use the address <span class="link__email">root&lt;at&gt;avatus&lt;dot&gt;dev</span>
 
 

9214306122619f76b1c20b11ac138c6699b30a3d
Michael <michaelzmyers@gmail.com>
2022-05-27 23:47:17 -0500

    adding color to prompt

--- src/components/ps1/Ps1.tsx
+++ src/components/ps1/Ps1.tsx
@@ -2,10 +2,12 @@ import React from "react";
 
 export const Ps1 = () => {
     return (
-        <div className="opacity-70">
-            <span className="">guest</span>
+        <div>
+            <span className="text-terminal-blue">guest</span>
             <span className="hidden md:inline-block">@</span>
-            <span className="hidden font-bold md:inline-block">avatus.dev</span>
+            <span className="hidden font-bold md:inline-block text-terminal-purple">
+                avatus.dev
+            </span>
             <span className="">:$ ~ </span>
         </div>
     );

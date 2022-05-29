b9c875ce79ae1607433a161f48466711e3ddffad
Michael <michaelzmyers@gmail.com>
2022-05-28 21:17:36 -0500

    updating about me

--- src/utils/bin/about.ts
+++ src/utils/bin/about.ts
@@ -2,7 +2,7 @@ const about = (): string => {
     return `
 My name is Michael Myers and I'm a fullstack software engineer.
 I build distributed, web-based applications.
-I'm an open-source advocate, fan of all-things-tech, and a ttrpg nerd.
+I'm a lifelong learner, an open-source advocate, and a fan of all things tech.
 I like to spend my free time <a class="link__teleport" href="https://github.com/avatus" target="_blank">coding</a>, <a class="link__teleport" href="https://soundcloud.com/avatus" target="_blank">making music</a>, and travelling to Japan.
 
 My current interests include Elixir, GraphQL federation, and RTC.
@@ -13,6 +13,7 @@ avatus
 シナプス
 
 --------------------------
+
 Type 'help' to see list of available commands.
 
     `;

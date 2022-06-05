9945485beca34042573735512e9efadfdc5287a8
Michael <michaelzmyers@gmail.com>
2022-06-05 13:22:31 -0500

    i like all federation, not just graphql

--- src/utils/bin/about.ts
+++ src/utils/bin/about.ts
@@ -5,7 +5,7 @@ I build distributed, web-based applications.
 I'm a lifelong learner, an open-source advocate, and a fan of all things tech.
 I like to spend my free time <a class="link__teleport" href="https://github.com/avatus" target="_blank">coding</a>, <a class="link__teleport" href="https://soundcloud.com/avatus" target="_blank">making music</a>, and travelling to Japan.
 
-My current interests include Elixir, GraphQL federation, and RTC.
+My current interests include Elixir, data federation, and RTC.
 For further communication, use the address <span class="link__email">root&lt;at&gt;avatus&lt;dot&gt;dev</span>
 
 
